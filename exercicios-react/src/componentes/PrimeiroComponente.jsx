071e1e10d31043cd5480162391c46d585caff3df
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2019-09-15 13:42:31 -0300

    Seção 2 - Fundamentos React - Primeiro Componente com Arrow

--- exercicios-react/src/componentes/PrimeiroComponente.jsx
+++ exercicios-react/src/componentes/PrimeiroComponente.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 
-export default function() {
-    return <h1>Primeiro Componente!</h1>;
-}
+export default () => 
+    <div>
+        <h1>Primeiro Componente (Arrow)!</h1>;
+    </div>
+
+
+//Pode fazer desse jeito também
+// export default function() {
+//     return <h1>Primeiro Componente!</h1>;
+// }
 
 //Pode fazer desse jeito também
 // function primeiro() {
