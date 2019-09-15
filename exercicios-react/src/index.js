84dac7ed211d3c5624ae09e111195498a8591a69
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2019-09-15 14:09:46 -0300

    Seção 2 - Fundamentos React - Um Arquivo com Múltiplos Componentes

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import PrimeiroComponente from './componentes/PrimeiroComponente';
+// o as serve para dar um apelido ao componente
+import { CompA, CompB as B} from './componentes/DoisComponentes';
 
 const elemento = document.getElementById('root');
 ReactDOM.render(
     <div>
         { /*O ** é  o operador de potência. Equivale a Math.pow(2, 8)*/}
-        <PrimeiroComponente valor = "Bom dia" potencia2={2**8}/>
+        <PrimeiroComponente valor="Bom dia" potencia2={2**8}/>
+
+        <CompA valor="Olá eu sou A!"/>
+        <B valor="B na área!"/>
     </div>
     , elemento);
 
