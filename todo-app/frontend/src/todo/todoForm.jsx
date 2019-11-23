5e03287dbf64113206dee157c928cefd0d3f69c7
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2019-11-22 21:06:30 -0300

    Seção 7: TodoApp - Migração para Redux - Action Creator de Pesquisa

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -1,41 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
-import { changeDescription } from './todoActions';
+import { changeDescription, search } from './todoActions';
 
-const TodoForm = props => {
-    const keyHandler = (event) => {
-        if(event.key === 'Enter') {
-            event.shiftKey ? props.handleSearch() : props.handleAdd();
-        } else if(event.key === 'Escape') {
-            props.handleClear();
-        }
+class TodoForm extends Component {
+    constructor(props) {
+        super(props)
+        this.keyHandler = this.keyHandler.bind(this);
     }
 
-    return (
-        <div role="form" className="todoForm">
-            <Grid cols="12 9 10">
-                <input id="description" 
-                    className="form-control" 
-                    placeholder="Adicione uma tarefa" 
-                    type="text"  
-                    value={props.description} 
-                    onChange={props.changeDescription} 
-                    onKeyUp={keyHandler} />
-            </Grid>
-    
-            <Grid cols="12 3 2">
-                <IconButton style="primary" icon="plus" onClick={props.handleAdd}></IconButton>
-                <IconButton style="info" icon="search" onClick={props.handleSearch}></IconButton>
-                <IconButton style="default" icon="close" onClick={props.handleClear}></IconButton>
-            </Grid>       
-        </div>
-    );
+    componentWillMount() {
+        this.props.search();
+    }
+
+    keyHandler(event) {
+        if(event.key === 'Enter') {
+            event.shiftKey ? this.props.handleSearch() : this.props.handleAdd();
+        } else if(event.key === 'Escape') {
+            this.props.handleClear();
+        } 
+    }
+
+    render() {
+        return(
+            <div role="form" className="todoForm">
+                <Grid cols="12 9 10">
+                    <input id="description" 
+                        className="form-control" 
+                        placeholder="Adicione uma tarefa" 
+                        type="text"  
+                        value={this.props.description} 
+                        onChange={this.props.changeDescription} 
+                        onKeyUp={this.keyHandler} />
+                </Grid>
+
+                <Grid cols="12 3 2">
+                    <IconButton style="primary" icon="plus" onClick={this.props.handleAdd}></IconButton>
+                    <IconButton style="info" icon="search" onClick={this.props.handleSearch}></IconButton>
+                    <IconButton style="default" icon="close" onClick={this.props.handleClear}></IconButton>
+                </Grid>       
+            </div>        
+        );
+    }
 }
 
 const mapStateToProps = state => ({ description: state.todo.description });
-const mapDispatchToProps = dispatch => bindActionCreators({changeDescription}, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription, search }, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
