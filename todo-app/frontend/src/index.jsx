7ac023f990715d5c27f113bf96ea563108482e9b
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2019-11-23 19:21:53 -0300

    Seção 7: TodoApp - Migração para Redux - Aplicando Middleware (Redux-Thunk)

--- todo-app/frontend/src/index.jsx
+++ todo-app/frontend/src/index.jsx
@@ -3,14 +3,19 @@ import ReactDom from 'react-dom';
 import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 
+/* Middleware de promise, toda vez que retorna uma promise ele espera a 
+promise ser resolvida para depois disparar os reducers. */
 import promise from 'redux-promise';
+/* Middleware de array de actions, que são executadas em paralelo. */
 import multi from 'redux-multi';
+/* Middleware que segura o retorno da action até que o processo assíncrono finalize. */
+import thunk from 'redux-thunk';
 
 import App from './main/app';
 import reducers from './main/reducers';
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-const store = applyMiddleware(multi, promise)(createStore)(reducers, devTools);
+const store = applyMiddleware(multi, promise, thunk)(createStore)(reducers, devTools);
 ReactDom.render(
     <Provider store={store}>
         <App />
