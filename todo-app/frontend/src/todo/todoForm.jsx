375d268db67e98bc1704d170f478c5808f3baa2b
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2019-11-23 22:30:06 -0300

    Seção 7: TodoApp - Migração para Redux - Limpar Formulário

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux';
 
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
-import { add, changeDescription, search } from './todoActions';
+import { add, changeDescription, clear, search } from './todoActions';
 
 class TodoForm extends Component {
     constructor(props) {
@@ -21,16 +21,16 @@ class TodoForm extends Component {
     }
 
     keyHandler(event) {
-        const { add, search, description } = this.props;
+        const { add, clear, search, description } = this.props;
         if(event.key === 'Enter') {
             event.shiftKey ? search() : add(description);
         } else if(event.key === 'Escape') {
-            this.props.handleClear();
+            clear();
         } 
     }
 
     render() {
-        const { add, search, description } = this.props;
+        const { add, clear, search, description } = this.props;
         return(
             <div role="form" className="todoForm">
                 <Grid cols="12 9 10">
@@ -46,7 +46,7 @@ class TodoForm extends Component {
                 <Grid cols="12 3 2">
                     <IconButton style="primary" icon="plus" onClick={() => add(description)}></IconButton>
                     <IconButton style="info" icon="search" onClick={() => search()}></IconButton>
-                    <IconButton style="default" icon="close" onClick={this.props.handleClear}></IconButton>
+                    <IconButton style="default" icon="close" onClick={()=> clear()}></IconButton>
                 </Grid>       
             </div>        
         );
@@ -55,5 +55,5 @@ class TodoForm extends Component {
 
 const mapStateToProps = state => ({ description: state.todo.description });
 const mapDispatchToProps = dispatch => 
-    bindActionCreators({ add, changeDescription, search }, dispatch);
+    bindActionCreators({ add, changeDescription, clear, search }, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
