10b83651aa6ad4d18bf765c549da90eb49dcf765
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-01-01 21:53:36 -0300

    Seção 9: Aplicação Final (Frontend) - Alterações Componente Rodapé

--- my-money-app/frontend/src/common/template/footer.jsx
+++ my-money-app/frontend/src/common/template/footer.jsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 export default props => (
     <footer className='main-footer'>
-        <div className='body-footer'>
-            <strong>
-                Copyright &copy; 2020
-                <a href='#' target='_blank'> Everton Cezar Gonçalves</a>.
-            </strong>
-        </div>
+        <strong>
+            Copyright &copy; 2020
+            <a href='#' target='_blank'> Everton Cezar Gonçalves</a>.
+        </strong>
     </footer>
 )
