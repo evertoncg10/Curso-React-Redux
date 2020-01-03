91afb6d3766c593462fa809f0f0132139024130d
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-01-03 18:42:16 -0300

    Seção 9: Aplicação Final (Frontend) - Criando a Primeira Action e o Reducer

--- my-money-app/frontend/src/main/reducers.js
+++ my-money-app/frontend/src/main/reducers.js
@@ -1,9 +1,11 @@
 import { combineReducers} from 'redux'
 
 import DashbordReducer from '../dashboard/dashboardReducer'
+import TabReducer from '../common/tab/tabReducer'
 
 const rootReducer = combineReducers({
-      dashboard: DashbordReducer
+      dashboard: DashbordReducer,
+      tab: TabReducer
 })
 
 export default rootReducer
