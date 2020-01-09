8b95b80793e09d17fd00dd142e0adfa63bb48122
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-01-08 21:50:28 -0300

    Seção 10: Melhorias e Correções - Navegação com React-router (Versão 2)

--- my-money-app/frontend/src/common/template/menuItem.jsx
+++ my-money-app/frontend/src/common/template/menuItem.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default props => (
     <li>
-        <a href={props.path}>
+        <Link to={props.path}>
             <i className={`fa fa-${props.icon}`}></i> <span>{props.label}</span>
-        </a>
+        </Link>
+        
+        {/* <a href={props.path}>
+            <i className={`fa fa-${props.icon}`}></i> <span>{props.label}</span>
+        </a> */}
     </li>
 )
